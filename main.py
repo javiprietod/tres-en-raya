3fbe1e13cd159ecaf21260453d0ee5ba840d4ac2
Alex <alex.m.guinea@alu.comillas.edu>
2023-02-06 11:32:13 +0100

    main

--- main.py
+++ main.py
@@ -1,20 +1,22 @@
 class Board:
     def __init__(self):
         self.state = '0'*9
-    
+
     def __str__(self):
-        XO = {0: ' ', 1: 'X', 2: 'O'}
-        str = '-------'
-        for i in range(3):
-            str += '|' + self.state[i*3:i*3+3] + '|'
-            for j in range(3):
-                str += self.state[i*3+j]
-            str += '-------'
-        str += '-------'
+        XO = {0: '   ', 1: ' X ', 2: ' O '}
+        str = '╔' + ('═'*3 + '╦')*2 + '═'*3 + '╗'
+        for idx, elem in enumerate(self.state):
+            if idx % 3 == 0:
+                str += '\n║'
+            str += XO[int(elem)] + '║'
+            if idx % 3 == 2 and idx != 8:
+                str += '\n╠' + ('═'*3 + '╬')*2 + '═'*3 + '╣'
+        str += '\n╚' + ('═'*3 + '╩')*2 + '═'*3 + '╝'
         return str
+
 
 class Agente:
     def __init__(self, board):
         self.board = board
         self.player = 2
-    
+        
